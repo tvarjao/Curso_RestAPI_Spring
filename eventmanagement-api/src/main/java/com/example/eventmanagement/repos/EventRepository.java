9732e2f69555ac4ef5362d6951b42121f945a1e3
Thiago Varjao <thiago.varjao@yahoo.com.br>
2019-01-21 18:50:49 -0300

    Add pagination to findByName result

--- eventmanagement-api/src/main/java/com/example/eventmanagement/repos/EventRepository.java
+++ eventmanagement-api/src/main/java/com/example/eventmanagement/repos/EventRepository.java
@@ -1,7 +1,7 @@
 package com.example.eventmanagement.repos;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
@@ -9,5 +9,5 @@ import com.example.eventmanagement.entities.Event;
 
 public interface EventRepository extends PagingAndSortingRepository<Event, Long> {
 
-	List<Event> findByName(@Param("name") String name);
+	Page<Event> findByName(@Param("name") String name, Pageable pageble);
 }
